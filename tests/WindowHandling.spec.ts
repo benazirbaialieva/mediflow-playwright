9413deceb04c9014c1c04d1f6e66ca3d7b6e9143
Benazir Baialieva <benazir.baialieva@gmail.com>
2026-05-14 11:05:39 -0700

    added alert management tests

--- tests/WindowHandling.spec.ts
+++ tests/WindowHandling.spec.ts
@@ -35,3 +35,82 @@ test('test multiple tabs', async ({ page, context }) =>{
 
 }
 )
+
+test('practice multiple tabs', async ({page, context}) => {
+    /**
+     * 1. go to https://the-internet.herokuapp.com/windows
+     * 2. click on "Click Here"
+     * 3. print title of new window
+     * 4. print the url of the main window
+     */
+    const mainPage = page;
+    await mainPage.goto('https://the-internet.herokuapp.com/windows');
+    const clickHereLink = mainPage.locator('a', { hasText: 'Click Here' });
+    
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),
+        await clickHereLink.click()
+    ]);
+    const newPageTitle = await newPage.title();
+    console.log('New page Title: ' + newPageTitle);
+    console.log('Main page URL: ' + mainPage.url());
+
+
+}
+)
+
+
+test('practice closing the tab', async ({page, context}) => {
+    /**
+     * 1. go to https://practice.expandtesting.com/windows
+     * 2. click on "Click Here"
+     * 3. on new page, verify text is displayed "Example of a new window page for Automation Testing Practice"
+     * 4. print the url of the main window
+     * 5. close new page
+     */
+    const mainPage = page;
+    await mainPage.goto('https://practice.expandtesting.com/windows');
+    const clickHereLink = mainPage.locator('a', { hasText: 'Click Here' });
+    
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),
+        await clickHereLink.click()
+    ]);
+    
+    const text = newPage.getByText('Example of a new window page for Automation Testing Practice');
+    await expect(text).toBeVisible();
+
+    console.log(page.url()); // main window
+    await newPage.close();
+
+
+}
+)
+
+test ('practice alert handling', async ({page}) => {
+/**
+ * 1. go to https://practice.expandtesting.com/js-dialogs
+ * 2. set up listener for alerts
+ * 3. click on JS Alert, print the text of alert and press Ok
+ * 4. click on JS confirm, print the text of alert and cancel the alert
+ * 5. click on JS prompt, print the text of alert, enter your name and press ok
+ */
+
+await page.goto('https://practice.expandtesting.com/js-dialogs');
+ page.on('dialog', async (dialog)=> {
+    console.log(dialog.message());
+
+    if (dialog.type() === 'alert'){
+        await dialog.accept();
+    }else if (dialog.type() === 'confirm'){
+        await dialog.dismiss();
+    }else if (dialog.type() === 'prompt'){
+        await dialog.accept('Bena');
+    }
+})
+
+  await page.getByRole("button", { name: "Js Alert" }).click();
+  await page.getByRole("button", { name: "Js Confirm" }).click();
+  await page.getByRole("button", { name: "Js Prompt" }).click();
+}
+)
